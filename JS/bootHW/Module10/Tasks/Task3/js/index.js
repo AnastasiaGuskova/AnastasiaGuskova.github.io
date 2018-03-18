f2eb5e5535c1fca23920b1a1f2fbed92d5962707
Asa Guskova <asa.guskova@gmail.com>
2018-03-18 13:51:58 +0200

    Add Module10 Task3

--- JS/bootHW/Module10/Tasks/Task3/js/index.js
+++ JS/bootHW/Module10/Tasks/Task3/js/index.js
@@ -8,3 +8,38 @@
   выбор в localStorage и если есть, то данные для подсветки 
   коавиши берутся оттуда.
 */
+
+// localStorage.clear();
+
+const btnsBody = document.querySelector('.lang-ctrls__body');
+
+let activeBtn = {
+	node: document.querySelector('.lang-ctrls__btn--active')
+}
+
+if (localStorage.getItem('lang')) {
+	let btns = document.querySelectorAll('.lang-ctrls__btn');
+	btns.forEach(function(item) {
+		if (item.dataset.lang === localStorage.getItem("lang")) {
+			changeHighlight(item);
+			activeBtn.node = item;
+		}
+	});
+}
+
+function changeHighlight(btn) {
+	activeBtn.node.classList.remove('lang-ctrls__btn--active');
+	btn.classList.add('lang-ctrls__btn--active');
+}
+
+function onClickLangBtn(event) {
+	let target = event.target;
+
+	if(target.tagName === 'BUTTON') {
+		changeHighlight(target);
+		activeBtn.node = target;
+		localStorage.setItem('lang', activeBtn.node.dataset.lang);
+	}
+}
+
+btnsBody.addEventListener('click', onClickLangBtn);
